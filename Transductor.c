541ed7f5d77bfe0edb72202797c015ab1d8331f7
Tiago Costa Arrazi <tiago_arrazi98@outlook.com>
2019-04-03 09:27:45 -0300

    Transductor is fully functioning

--- Transductor.c
+++ Transductor.c
@@ -5,6 +5,7 @@
 char tape[255]; int p=0;
 char tempS[20]; int t=0;
 char tabVar[20][20]; int k=0;
+unsigned long long tempN;
 
 void copy(char d[], char s[]);
 int equal(char u[], char d[]);
@@ -12,10 +13,14 @@ void printTabVar();
 
 void s0(); // initial and final state
 void s1();
+void s2();
 
 void sigma1(); // tempS[0] <- symbol
 void sigma2(); // tempS += symbol
-void sigma3(); // checks whether tempS is in tabVar
+void sigma3(); // checks whether tempS is in tabVar and shows V()
+void sigma4(); // tempN <- symbol - '0'
+void sigma5(); // tempN <- tempN * 10 + (symbol - '0')
+void sigma6(); // shows N()
 
 int main(int argc, char *argv[]) {
     
@@ -31,7 +36,12 @@ void s0() {
         sigma1();
     
     }
-    else if(tape[p] == '\0'){
+    
+    else if(tape[p] >= '0' && tape[p] <= '9') {
+    	sigma4();	
+	}
+    
+    else if(tape[p] == '\0') {
     	printf("\n\n");
         printTabVar();
         exit(0);
@@ -54,7 +64,19 @@ void s1() {
     }   
 }
 
-void sigma1(){
+void s2() {
+	
+	if(tape[p] >= '0' && tape[p] <= '9') {
+		sigma5();
+	}
+	
+	else {
+		sigma6();
+	}
+	
+}
+
+void sigma1() {
     
     tempS[t] = tape[p];
     p++;
@@ -64,7 +86,7 @@ void sigma1(){
 }
 
 
-void sigma2(){
+void sigma2() {
     
     tempS[t] = tape[p];
     p++;
@@ -74,7 +96,7 @@ void sigma2(){
 }
 
 
-void sigma3(){
+void sigma3() {
     
     tempS[t] = '\0';
     
@@ -82,29 +104,55 @@ void sigma3(){
     int flag = 0;
     int pos;
 
-    for(c=0; tabVar[c][0]!='\0'; c++){
+    for(c=0; tabVar[c][0]!='\0'; c++) {
 	
-        if(equal(tempS, tabVar[c]) == 1){
+        if(equal(tempS, tabVar[c]) == 1) {
 	    flag = 1;
 	    pos = c;
 
-	}
+		}
     }
 
-    if(flag == 1){
-        printf("VAR(%d) ", pos);
+    if(flag == 1) {
+        printf("V(%d) ", pos);
    
     }
 
-    else{
-        copy(tabVar[k], tempS);
-	printf("VAR(%d) ", k);
-	k++;
+    else {
+    	copy(tabVar[k], tempS);
+		printf("V(%d) ", k);
+		k++;
     
     }
 
     t = 0;
-    s0();   
+    s0(); 
+	  
+}
+
+void sigma4() {
+	
+	tempN = tape[p] - '0';
+	p++;
+	s2();
+	
+}
+
+
+void sigma5() {
+	
+	tempN = tempN * 10 + (tape[p] - '0');
+	p++;
+	s2();
+	
+}
+
+
+void sigma6() {
+	
+	printf("N(%d) ", tempN);
+	s0();
+	
 }
 
 
@@ -121,23 +169,21 @@ int equal(char u[], char d[]) {
     int i = 0;
     int flag = 0;
     
-    while(u[i] != '\0' && d[i] != '\0'){
+    while(u[i] != '\0' && d[i] != '\0') {
     	
-    	if(u[i] != d[i]){
+    	if(u[i] != d[i]) {
             flag = 1;
 	}
 
     	i++;
     }
 	
-	if(flag == 0 && u[i] == '\0' && d[i] == '\0'){
-	    //printf("\n%s & %s are equal\n", u, d);
-            return 1;
-	}
-    	else{
-    	    //printf("\n%s & %s are NOT equal\n", u, d);
-            return 0;
-	}
+	if(flag == 0 && u[i] == '\0' && d[i] == '\0')
+        return 1;
+	
+    else
+        return 0;
+	
     
 }
 
